82308fe42646f9bdb5711ba8b206e02af3015c14
Darryl Leong <darryleong95@gmail.com>
2020-04-24 18:58:26 +0800

    Updates

---
+++ precache-manifest.9fca5782ed9bf3411463d4d9e9acd896.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d3e3d8ad2370d55ce544a46183f7c6c",
+    "url": "/darryls-todos/index.html"
+  },
+  {
+    "revision": "814a2a5f0798d50e9039",
+    "url": "/darryls-todos/static/css/main.64608f9b.chunk.css"
+  },
+  {
+    "revision": "66c0e91c9b31e24a31c2",
+    "url": "/darryls-todos/static/js/2.ad8f4655.chunk.js"
+  },
+  {
+    "revision": "86414f6aa96d78f04460ff2dda206a29",
+    "url": "/darryls-todos/static/js/2.ad8f4655.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "814a2a5f0798d50e9039",
+    "url": "/darryls-todos/static/js/main.4a527526.chunk.js"
+  },
+  {
+    "revision": "10ffc9e7049440e59772",
+    "url": "/darryls-todos/static/js/runtime-main.da6d77a6.js"
+  },
+  {
+    "revision": "316c232187bac46e708fad0b5d83666f",
+    "url": "/darryls-todos/static/media/Quicksand-Regular.316c2321.ttf"
+  }
+]);
